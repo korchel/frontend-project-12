ef5bd0b4c8682637e7a68058a4cb8e1a87df855f
Elena Korchuganova <korchel21@gmail.com>
2023-08-12 15:33:30 +0300

    messages scroll added

--- frontend/src/components/chatPage/components/MessagesBox.jsx
+++ frontend/src/components/chatPage/components/MessagesBox.jsx
@@ -1,12 +1,21 @@
-const MessagesBox = ({ messages }) => (
-  <div id="messages-box" className="chat-messages overflow-auto px-5">
-    {messages.map((message) => (
-      <div key={message.id} className="text-break mb-2">
-        <b>{message.username}</b>
-        {`:${message.body}`}
-      </div>
-    ))}
-  </div>
-);
+import React, { useEffect } from 'react';
+import { animateScroll } from "react-scroll";
+
+const MessagesBox = ({ messages }) => {
+  useEffect(() => {
+    animateScroll.scrollToBottom({containerId: 'messages-box', duration: 0});
+  });
+
+  return (
+    <div id="messages-box" className="chat-messages overflow-auto px-5">
+      {messages.map((message) => (
+        <div key={message.id} className="text-break mb-2">
+          <b>{message.username}</b>
+          {`:${message.body}`}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default MessagesBox;
