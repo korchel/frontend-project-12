7259c2caf035d6f342867428ab352040425f1a22
korchel <korchel21@gmail.com>
2023-08-22 16:18:16 +0300

    notFound picture replaced

--- frontend/src/components/notFoundPage/NotFoundPage.jsx
+++ frontend/src/components/notFoundPage/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import image from '../../assets/notFound.svg';
+import image from '../../assets/notFound.jpg';
 import routes from '../../routes';
 
 const NotfoundPage = () => {
